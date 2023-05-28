b3b7bce7ec0503b2fdc1465693967326caddcbbd
SeanHuang <sean02050923@gmail.com>
2023-05-28 12:22:16 +0800

    update

---
+++ server/validation.js
@@ -0,0 +1,31 @@
+const Joi = require("joi");
+// Register Validation
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(6).max(100).required().email(),
+    password: Joi.string().min(6).max(1024).required(),
+  });
+
+  return schema.validate(data);
+};
+
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(50).required().email(),
+    password: Joi.string().min(6).max(1024).required(),
+  });
+  return schema.validate(data);
+};
+
+const journalValidation = (data) => {
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+  });
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
+module.exports.journalValidation = journalValidation;
